71f177ea8c6191960de2f6d64d680fef4ac5f511
8BitSummoner <harry10potter1@gmail.com>
2019-07-31 16:56:49 -0700

    Added check for gliding with elytra

--- src/main/java/ca/avalonmc/avnelytras/AvNElytras.java
+++ src/main/java/ca/avalonmc/avnelytras/AvNElytras.java
@@ -1,20 +1,39 @@
 package ca.avalonmc.avnelytras;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 
 public final class AvNElytras extends JavaPlugin {
 	
+	Logger thisismylogger = getLogger();
+	
 	@Override
 	public void onEnable () {
-		// Plugin startup logic
+		
+		thisismylogger.info("Plugin Enabled! Good morning :)");
 		
 	}
 	
-	
-	@Override
-	public void onDisable () {
-		// Plugin shutdown logic
+	@EventHandler
+	public void onGliding(EntityToggleGlideEvent e){
+		
+		Player player = (Player)e.getEntity();
+		
+		if(player.isGliding()){
+			
+			player.sendMessage("Hey bish ur flyijing");
+			
+		} else {
+			
+			player.sendMessage("omg ur landed");
+			
+		}
+		
 	}
 	
 }
