43a5672b5bb5407a97e2672e8907356ec9b8cef4
JPEG223 <jpegmc223@gmail.com>
2019-08-01 21:56:55 -0400

    Added Toggle Elytras in ElytraToggle.java

--- src/main/java/ca/avalonmc/avnelytra/commands/ElytraToggle.java
+++ src/main/java/ca/avalonmc/avnelytra/commands/ElytraToggle.java
@@ -5,22 +5,29 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ElytraToggle implements CommandExecutor {
-	
+
+	List<String> elytratoggle = new ArrayList<String>();
 	public boolean onCommand (CommandSender sender, Command command, String label, String[] args) {
-		
-		if (!(sender instanceof Player)) {
-			
-			sender.sendMessage("You need to be a Player to do this command");
-			return false;
-			
+		if (label.equalsIgnoreCase("elytratoggle")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("You need to be a Player to do this command");
+				return false;
+			}
+			Player player = (Player) sender;
+			if (elytratoggle.contains(player.getName())) {
+				player.sendMessage("§2You §uDISABLED§2 Elytras!");
+				return true;
+			}
+
+			player.sendMessage("§2You §uENABLED§2 Elytras!");
+			elytratoggle.add(player.getName());
+			return true;
 		}
-		
-		Player player = (Player)sender;
-		player.sendMessage("§2You §uDISABLED§2 Elytras!");
-		return true;
-		
+		return false;
 	}
-	
 }
