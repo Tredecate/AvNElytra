9e4d632fef0d38116e36cad4337b500d8cc01d1a
8BitSummoner <harry10potter1@gmail.com>
2019-08-01 23:16:31 -0700

    Finalized elytratoggleall and added to plugin.yml and added comments

--- src/main/java/ca/avalonmc/avnelytra/commands/ElytraToggleAll.java
+++ src/main/java/ca/avalonmc/avnelytra/commands/ElytraToggleAll.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 
 import static ca.avalonmc.avnelytra.AvNElytra.globalelytratoggle;
 import static ca.avalonmc.avnelytra.AvNElytra.log;
+import static org.bukkit.Bukkit.getServer;
 
 
 public class ElytraToggleAll implements CommandExecutor {
@@ -17,24 +18,21 @@ public class ElytraToggleAll implements CommandExecutor {
 			
 			if (globalelytratoggle) {
 				
-				sender.sendMessage("§2You §nDISABLED§2 Elytras!");
+				sender.sendMessage("§2You §nDISABLED§2 Elytras in SERVER: " + getServer().getName());
 				log.info(sender.getName() + " disabled elytra flight.");
 				
-				return true;
-				
-			}
-			else {
-				
-				sender.sendMessage("§2You §nENABLED§2 Elytras!");
-				log.info(sender.getName() + " enabled elytra flight.");
-				
-				return true;
+				return true; // Global elytra was on, now it's off
 				
 			}
 			
+			sender.sendMessage("§2You §nENABLED§2 Elytras in SERVER: " + getServer().getName());
+			log.info(sender.getName() + " enabled elytra flight.");
+			
+			return true; // Global elytra was off, now it's on
+			
 		}
 		
-		return false;
+		return false; // COMMAND FAILED
 		
 	}
 	
