2be3c57762bccd1ef803b62b90361fc5ec82b1f9
8BitSummoner <thomasdezwart@gmail.com>
2019-09-26 19:57:42 -0700

    Added use of a config.yml to set and store plugin settings.

--- src/main/java/ca/avalonmc/avnelytra/AvNElytra.java
+++ src/main/java/ca/avalonmc/avnelytra/AvNElytra.java
@@ -1,14 +1,15 @@
 package ca.avalonmc.avnelytra;
 
 import ca.avalonmc.avnelytra.commands.ElytraToggle;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
 import java.util.logging.Logger;
 
 
 public final class AvNElytra extends JavaPlugin {
 	
+	private static FileConfiguration config;
 	public static Logger log;
 	
 	public static Boolean globalelytratoggle = true;
@@ -29,6 +30,8 @@ public final class AvNElytra extends JavaPlugin {
 	@Override
 	public void onDisable () {  //ON PLUGIN DISABLE
 		
+		this.saveConfig();
+		
 		log.info("===========================");
 		log.info("AvNElytra: Plugin Disabled!");
 		log.info("===========================");
@@ -38,6 +41,12 @@ public final class AvNElytra extends JavaPlugin {
 	
 	public void registerComponents () {
 		
+		// Create config file
+		saveDefaultConfig();
+		
+		// Get config
+		config = this.getConfig();
+		
 		// Register event handler
 		getServer().getPluginManager().registerEvents(new EventListener(), this);
 		
@@ -46,6 +55,20 @@ public final class AvNElytra extends JavaPlugin {
 		
 		// Instantiate logger
 		log = getLogger();
+		
+	}
+	
+	
+	public FileConfiguration getCurrentConfig () {
+		
+		return config;
+		
+	}
+	
+	
+	public void saveCurrentConfig () {
+		
+		getCurrentConfig().set("ElytraDisabled", globalelytratoggle);
 		
 	}
 	
