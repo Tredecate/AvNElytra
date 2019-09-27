e0cf1c42a85b6080326a6a49adef1b67642a5e53
8BitSummoner <thomasdezwart@gmail.com>
2019-09-26 20:14:42 -0700

    Removed last traces of old ElytraToggle

--- src/main/java/ca/avalonmc/avnelytra/EventListener.java
+++ src/main/java/ca/avalonmc/avnelytra/EventListener.java
@@ -3,11 +3,8 @@ package ca.avalonmc.avnelytra;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.entity.EntityToggleGlideEvent;
-import org.bukkit.event.player.PlayerRiptideEvent;
 
-import static ca.avalonmc.avnelytra.AvNElytra.elytratoggle;
 import static ca.avalonmc.avnelytra.AvNElytra.globalelytratoggle;
 
 
@@ -19,7 +16,7 @@ public class EventListener implements Listener {
 		
 		Player player = (Player)e.getEntity();
 		
-		if (e.isGliding() && (!elytratoggle.contains(player.getName()) || !globalelytratoggle)) {
+		if (e.isGliding() && (!globalelytratoggle)) {
 			
 			player.sendMessage("§cYou are not allowed to fly!");
 			e.setCancelled(true);
