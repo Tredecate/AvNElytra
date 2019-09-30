90c8b3f54cf494519ba6850eb2a4f7e95adde057
8BitSummoner <thomasdezwart@gmail.com>
2019-09-29 17:03:17 -0700

    Updated RocketBoostToggle to fit new command system and format.

--- src/main/java/ca/avalonmc/avnelytra/commands/RocketBoostToggle.java
+++ src/main/java/ca/avalonmc/avnelytra/commands/RocketBoostToggle.java
@@ -4,35 +4,56 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import static ca.avalonmc.avnelytra.AvNElytra.log;
-import static ca.avalonmc.avnelytra.AvNElytra.rocketBoostDisabled;
-import static org.bukkit.Bukkit.getServer;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static ca.avalonmc.avnelytra.AvNElytra.*;
 
 
 public class RocketBoostToggle implements CommandExecutor {
 	
+	private ArrayList<String> labels;
+	
+	
+	public RocketBoostToggle (String... labels) {
+		
+		this.labels = new ArrayList<String>(Arrays.asList(labels));
+		
+	}
 	
 	public boolean onCommand (CommandSender sender, Command command, String label, String[] args) {
 		
-		if (label.equalsIgnoreCase("rocketboosttoggle")) {
+		if (labels.contains(label.toLowerCase())) {
 			
-			if (!rocketBoostDisabled) {
+			if (args.length < 1) {
 				
-				sender.sendMessage("§2You §nDISABLED§2 rocket boosting in SERVER: " + getServer().getName());
-				log.info(sender.getName() + " disabled rocket boosting.");
+				sender.sendMessage(errorPrefix + "Firework rocket boosting is currently §d" + (rocketBoostDisabled ? "disabled" : "enabled") + "§7.");
+				return true;
+				
+			}
+			
+			if (on.contains(args[0].toLowerCase()) && rocketBoostDisabled) {
+				
+				sender.sendMessage(errorPrefix + "You §denabled §7firework rocket boosting.");
+				log.info(sender.getName() + " enabled firework rocket boosting.");
+				
+				rocketBoostDisabled = false;
+				return true; // Rocket boosting was off, now it's on
+				
+			}
+			
+			if (off.contains(args[0].toLowerCase()) && !rocketBoostDisabled) {
+				
+				sender.sendMessage(errorPrefix + "You §ddisabled §7firework rocket boosting.");
+				log.info(sender.getName() + " disabled firework rocket boosting.");
 				
 				rocketBoostDisabled = true;
-				
 				return true; // Rocket boosting was on, now it's off
 				
 			}
 			
-			sender.sendMessage("§2You §nENABLED§2 rocket boosting in SERVER: " + getServer().getName());
-			log.info(sender.getName() + " disabled rocket boosting.");
-			
-			rocketBoostDisabled = false;
-			
-			return true; // Rocket boosting was off, now it's on
+			sender.sendMessage(errorPrefix + "Incorrect usage. Please refer to §d/avnelytra help§7.");
+			return false;
 			
 		}
 		
