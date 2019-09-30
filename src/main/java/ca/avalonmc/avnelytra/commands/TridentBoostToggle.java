7842f157d97cfcc9c58fad6e9d6b513e56e98f9c
8BitSummoner <thomasdezwart@gmail.com>
2019-09-29 17:06:41 -0700

    Updated TridentBoostToggle to fit new command system and format.

--- src/main/java/ca/avalonmc/avnelytra/commands/TridentBoostToggle.java
+++ src/main/java/ca/avalonmc/avnelytra/commands/TridentBoostToggle.java
@@ -4,35 +4,56 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import static ca.avalonmc.avnelytra.AvNElytra.log;
-import static ca.avalonmc.avnelytra.AvNElytra.tridentBoostDisabled;
-import static org.bukkit.Bukkit.getServer;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static ca.avalonmc.avnelytra.AvNElytra.*;
 
 
 public class TridentBoostToggle implements CommandExecutor {
 	
+	private ArrayList<String> labels;
+	
+	
+	public TridentBoostToggle (String... labels) {
+		
+		this.labels = new ArrayList<String>(Arrays.asList(labels));
+		
+	}
 	
 	public boolean onCommand (CommandSender sender, Command command, String label, String[] args) {
 		
-		if (label.equalsIgnoreCase("tridentboosttoggle")) {
+		if (labels.contains(label.toLowerCase())) {
 			
-			if (!tridentBoostDisabled) {
+			if (args.length < 1) {
 				
-				sender.sendMessage("§2You §nDISABLED§2 trident boosting in SERVER: " + getServer().getName());
+				sender.sendMessage(errorPrefix + "Trident boosting is currently §d" + (tridentBoostDisabled ? "disabled" : "enabled") + "§7.");
+				return true;
+				
+			}
+			
+			if (on.contains(args[0].toLowerCase()) && tridentBoostDisabled) {
+				
+				sender.sendMessage(errorPrefix + "You §denabled §7trident boosting.");
+				log.info(sender.getName() + " enabled trident boosting.");
+				
+				tridentBoostDisabled = false;
+				return true; // Trident boosting was off, now it's on
+				
+			}
+			
+			if (off.contains(args[0].toLowerCase()) && !tridentBoostDisabled) {
+				
+				sender.sendMessage(errorPrefix + "You §ddisabled §7trident boosting.");
 				log.info(sender.getName() + " disabled trident boosting.");
 				
 				tridentBoostDisabled = true;
-				
 				return true; // Trident boosting was on, now it's off
 				
 			}
 			
-			sender.sendMessage("§2You §nENABLED§2 trident boosting in SERVER: " + getServer().getName());
-			log.info(sender.getName() + " disabled trident boosting.");
-			
-			tridentBoostDisabled = false;
-			
-			return true; // Trident boosting was off, now it's on
+			sender.sendMessage(errorPrefix + "Incorrect usage. Please refer to §d/avnelytra help§7.");
+			return false;
 			
 		}
 		
