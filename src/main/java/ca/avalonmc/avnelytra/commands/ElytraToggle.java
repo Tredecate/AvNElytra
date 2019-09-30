d936c589e6918e75db3a7dd2abb8ccc274ca9c3c
8BitSummoner <thomasdezwart@gmail.com>
2019-09-29 17:17:17 -0700

    Updated ElytraToggle to fit new command system and format.

--- src/main/java/ca/avalonmc/avnelytra/commands/ElytraToggle.java
+++ src/main/java/ca/avalonmc/avnelytra/commands/ElytraToggle.java
@@ -5,24 +5,49 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import static ca.avalonmc.avnelytra.AvNElytra.elytraDisabled;
-import static ca.avalonmc.avnelytra.AvNElytra.log;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static ca.avalonmc.avnelytra.AvNElytra.*;
 import static org.bukkit.Bukkit.getServer;
 
 
 public class ElytraToggle implements CommandExecutor {
 	
+	private ArrayList<String> labels;
+	
+	
+	public ElytraToggle (String... labels) {
+		
+		this.labels = new ArrayList<String>(Arrays.asList(labels));
+		
+	}
 	
 	public boolean onCommand (CommandSender sender, Command command, String label, String[] args) {
 		
-		if (label.equalsIgnoreCase("elytratoggle")) {
+		if (labels.contains(label.toLowerCase())) {
 			
-			if (!elytraDisabled) {
+			if (args.length < 1) {
 				
-				sender.sendMessage("§2You §nDISABLED§2 Elytras in SERVER: " + getServer().getName());
+				sender.sendMessage(errorPrefix + "Elytra flight is currently §d" + (elytraDisabled ? "disabled" : "enabled") + "§7.");
+				return true;
+				
+			}
+			
+			if (on.contains(args[0].toLowerCase()) && !elytraDisabled) {
+				
+				sender.sendMessage(errorPrefix + "You §denabled §7elytra flight.");
+				log.info(sender.getName() + " enabled elytra flight.");
+				
+				elytraDisabled = false;
+				return true; // Elytra flight was off, now it's on
+				
+			}
+			
+			if (off.contains(args[0].toLowerCase()) && !elytraDisabled) {
+				
+				sender.sendMessage(errorPrefix + "You §ddisabled §7elytra flight.");
 				log.info(sender.getName() + " disabled elytra flight.");
-				
-				elytraDisabled = true;
 				
 				for (Player p : getServer().getOnlinePlayers()) {
 					
@@ -30,16 +55,13 @@ public class ElytraToggle implements CommandExecutor {
 					
 				}
 				
+				elytraDisabled = true;
 				return true; // Elytra flight was on, now it's off
 				
 			}
 			
-			sender.sendMessage("§2You §nENABLED§2 Elytras in SERVER: " + getServer().getName());
-			log.info(sender.getName() + " enabled elytra flight.");
-			
-			elytraDisabled = false;
-			
-			return true; // Elytra flight was off, now it's on
+			sender.sendMessage(errorPrefix + "Incorrect usage. Please refer to §d/avnelytra help§7.");
+			return false;
 			
 		}
 		
